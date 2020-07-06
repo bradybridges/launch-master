bd361bc93ab1b68ce36ccd9d15e2880d96445e13
Brady Bridges <bradyjbridges@gmail.com>
2020-07-06 00:04:30 -0500

    Add fetch for past launches

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 export const getUpcomingLaunches = async (num) => {
-  try{
+  try {
     const response = await fetch(`https://launchlibrary.net/1.3/launch/next/${num}`);
     if(response.status === 200) {
       const launches = await response.json();
@@ -7,5 +7,18 @@ export const getUpcomingLaunches = async (num) => {
     }
   } catch(e) {
     return new Error('There was a problem fetching upcoming launches');
+  }
+}
+
+export const getPastLaunches = async (windowStart, windowEnd) => {
+  try {
+    const response = await fetch(`https://launchlibrary.net/1.3/launch/${windowStart}/${windowEnd}`);
+
+    if(response.status === 200) {
+      const launches = await response.json();
+      return launches;
+    }
+  } catch(e) {
+    return new Error('There was a problem retrieving past launches');
   }
 }
