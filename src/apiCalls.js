2d86235563db321b7ac568690fc615de9e17023e
Brady Bridges <bradyjbridges@gmail.com>
2020-07-14 16:58:01 -0500

    Add condition to make sure response is okay and has status 200 to not throw error

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,7 @@
 export const getUpcomingLaunches = async (num) => {
   try {
     const response = await fetch(`https://launchlibrary.net/1.3/launch/next/${num}`);
-    if(response.status === 200) {
+    if(response.status === 200 || !response.ok) {
       const launches = await response.json();
       return launches;
     }
@@ -14,7 +14,7 @@ export const getPastLaunches = async (windowStart, windowEnd) => {
   try {
     const response = await fetch(`https://launchlibrary.net/1.3/launch/${windowStart}/${windowEnd}`);
 
-    if(response.status === 200) {
+    if(response.status === 200 || !response.ok) {
       const launches = await response.json();
       return launches;
     }
